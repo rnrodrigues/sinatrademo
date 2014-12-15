f2e75325785ec77ead2210c2087595c3b745c520
rnrodrigues <rnrodrigs@gmail.com>
2014-12-15 17:15:45 +0000

    add .gitignore

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
 require 'sinatra/base'
 
 module MyApi
-  class App < Sinatra::Base
-    get '/hello' do
-      "Hi"
+    class App < Sinatra::Base
+        get '/hello' do
+            "Hi"
+        end
+        
+        get '/' do
+            # Excuse me! I don't want to think.
+            send_file File.join('public', 'index.html')
+        end
+        
+        get '/api/v1/pong' do
+            "Pong!"
+        end
     end
-    
-    get '/' do
-      # Excuse me! I don't want to think.
-      send_file File.join('public', 'index.html')
-    end
-  end
 end
